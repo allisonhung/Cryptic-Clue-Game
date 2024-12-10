7b47fee27a539704bf723639e7cf9a02787397e4
TessHutchinson <165493472+TessHutchinson@users.noreply.github.com>
2024-12-09 22:10:53 -0800

    Updated How To Play

--- test/src/components/HowToPlay.tsx
+++ test/src/components/HowToPlay.tsx
@@ -1,19 +1,44 @@
-import { Devvit } from "@devvit/public-api";
-type PageProps = {
-  setPage: (page: string) => void;
-};
+import {Devvit, } from '@devvit/public-api'
 
-const HowToPlay = ({ setPage }: PageProps) => (
-  <vstack
+Devvit.addCustomPostType({
+  name: 'Name',
+  render: context => {
+      return (
+     <zstack 
+       height="100%" width="100%" alignment="center middle">
+    //background
+       <image
+      url="wood_background.jpg"
+      description="wooden background"
+      imageHeight={1024}
+      imageWidth={2048}
+      height="100%"
+      width="100%"
+      resizeMode="cover" />
+       <vstack
     width="100%"
     height="100%"
     alignment="middle center"
-    gap="large"
-    backgroundColor="lightblue"
+    gap="medium"
   >
-    <text size="xxlarge">How to play page</text>
+    <text size="xxlarge" color="white" outline="thick">Rules!</text>
+   <vstack> 
+     <spacer size="small" /> 
+     <text color="white" outline="thin"> The clue-giver provides a hint along with the number of cards it applies to. </text>
+     <spacer size ="xsmall" />
+       <text color="white" outline="thin">The guesser then tries to identify which cards are linked to the given clue.</text>
+       <spacer size="xsmall" />
+     <text color="white" outline="thin">Example: Clue: "Beach" -2  </text>
+     <spacer size="xsmall" />
+     <text color="white" outline="thin">Cards: "Sand," "Ocean," "Star," "Robot," "Whale" </text>
+      <spacer size="xsmall" />
+     <text color="white" outline="thin">Guesses: "Sand" and "Ocean"</text>
+     </vstack>
     <button onPress={() => setPage('Home')}>Back to menu</button>
-  </vstack>
-);
+  </vstack> 
+       </zstack>
+  )
+  }
+})
 
-export default HowToPlay;
+export default Devvit
